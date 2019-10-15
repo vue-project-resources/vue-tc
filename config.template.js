5628df09918c067c88a7430fc20b3eda6624fa02
huxiaocong <huxc10086@163.com>
2019-10-15 12:49:56 +0800

    修改配置文件

--- config.template.js
+++ config.template.js
@@ -1,9 +1,24 @@
 module.exports = {
-    pages: {
-        developer: {
-            entry: "src/developer/main.js",
-            template: "public/index.html",
-            filename: "index.html"
-        }
-    }
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080, // 端口号
+    disableHostCheck: true,
+    open: true, // 配置自动启动浏览器
+    hotOnly: true // 热更新
+    // proxy: {
+    // '/': {
+    //   target: 'http://wwenj.com',
+    //   changeOrigin: true,
+    //   ws: false,
+    //   bypass: (req, res) => {
+    //     if (req.url === '/') return `/static`
+    //     // if (/\.\w+$/.test(req.url)) {
+    //     //   console.log('skip static', req.url)
+    //     //   return req.url
+    //     // }
+    //   }
+    // }
+    // }
+  },
+  pages: {}
 };
